90e2d1e9fe70845c92bce8cd933e827c1e9b3db9
kautuk <kautukkundan@gmail.com>
2018-01-18 23:41:32 +0530

    take-a-break app added

---
+++ take-a-break.py
@@ -0,0 +1,6 @@
+import webbrowser
+import time
+
+while(True):
+	time.sleep(5)
+	webbrowser.open("https://www.youtube.com/watch?v=s8XIgR5OGJc&list=PLC1og_v3eb4hrv4wsqG1G5dsNZh9bIscJ&index=1", new=2)
