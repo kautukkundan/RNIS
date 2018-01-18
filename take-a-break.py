65e8d2e2f92af79a0bc757c133e06e7e527e81f7
kautuk <kautukkundan@gmail.com>
2018-01-18 23:54:18 +0530

    commented take-a-break app

--- take-a-break.py
+++ take-a-break.py
@@ -1,6 +1,8 @@
-import webbrowser
-import time
+import webbrowser 		#imports webbrowser module
+import time				#imports time module
 
 while(True):
-	time.sleep(5)
+	time.sleep(5)		#waits for 5 seconds before goinf to next line
+
+	#opens this link in a new tab of browser
 	webbrowser.open("https://www.youtube.com/watch?v=s8XIgR5OGJc&list=PLC1og_v3eb4hrv4wsqG1G5dsNZh9bIscJ&index=1", new=2)
